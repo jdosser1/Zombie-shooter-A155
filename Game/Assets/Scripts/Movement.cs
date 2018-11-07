2c23850cef946f2cc7d639749217cef758a36bd9
jdosser1 <43547992+jdosser1@users.noreply.github.com>
2018-11-07 09:50:16 +0000

    Movement

---
+++ Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+}
