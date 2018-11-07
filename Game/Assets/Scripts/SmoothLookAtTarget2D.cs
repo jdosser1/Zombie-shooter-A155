6e5bb3a754ca31a55f3c93ec5ab96f8fec49b7b0
jdosser1 <43547992+jdosser1@users.noreply.github.com>
2018-11-07 10:05:26 +0000

    Movement fix and Blood Start

--- Game/Assets/Scripts/SmoothLookAtTarget2D.cs
+++ Game/Assets/Scripts/SmoothLookAtTarget2D.cs
@@ -12,7 +12,7 @@ public class SmoothLookAtTarget2D : MonoBehaviour {
     {
         if (target != null)
         {
-            Vector3 difference = target.position = transform.position;
+            Vector3 difference = target.position - transform.position;
             float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             Quaternion newRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
             transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothing);
