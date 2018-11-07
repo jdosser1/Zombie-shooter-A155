3467da57ae9bf39c4f2a1db4f2627f5f583a503b
jdosser1 <43547992+jdosser1@users.noreply.github.com>
2018-11-07 09:20:08 +0000

    Health

---
+++ Game/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int> { }
+public class HealthSystem : MonoBehaviour {
+
+    public int health = 10;
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+
+    public void TakeDamage( int damage)
+    {
+        health -= damage;
+
+        onDamaged.Invoke(health);
+
+            if (health < 1) {
+        
+            onDie.Invoke() ;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
