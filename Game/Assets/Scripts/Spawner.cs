ad573194809db8b018ee9f1ec1d98ecf01900cef
jdosser1 <43547992+jdosser1@users.noreply.github.com>
2018-11-14 10:10:26 +0000

    Spawners

---
+++ Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Spawner : MonoBehaviour {
+
+    public float time = 1;
+    public bool repeat = false;
+    public UnityEvent onTimerComplete;
+
+    private void start()
+    {
+        if (repeat)
+        {
+            InvokeRepeating("OnTimerComplete", 0, time);
+        }
+        else
+        {
+            invoke("OnTimerComplete", time);
+        }
+
+    }
+ 
+        private void OnTimerComplete()
+        {
+            onTimerComplete.Invoke();
+        }
+    
+}
