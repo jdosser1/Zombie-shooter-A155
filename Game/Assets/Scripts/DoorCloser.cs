cb6981583aefbc6146125823b354551d5fc60a1f
jdosser1 <43547992+jdosser1@users.noreply.github.com>
2018-12-05 11:02:24 +0000

    Door
    Boss Door

---
+++ Game/Assets/Scripts/DoorCloser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorCloser : MonoBehaviour {
+
+    public Transform door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+        door.gameObject.SetActive(true);
+	}
+}
